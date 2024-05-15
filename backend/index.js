7dfc9f714ea13774c69d1d57eb08b76ec2c552b6
Enrika <e.rudzikiene@gmail.com>
2024-05-15 23:00:43 +0300

    Update routes and API endpoints for tasks, users, and priorities

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const userRoutes = require("../backend/router/usersRouter");
 const tasksRoutes = require("../backend/router/taskRouter");
 const { verifyToken } = require("../backend/middlewares/auth");
 const auth = require("../backend/router/authRouter");
+const priority = require("../backend/router/priorityRouter");
 
 const app = express();
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 8081;
 
 app.use(userRoutes, verifyToken);
 app.use(tasksRoutes, verifyToken);
+app.use(priority, verifyToken);
 app.use(auth);
 
 app.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import AppRoutes from './routes/AppRoutes';
 import { UserProvider } from '../src/contexts/UserContext';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const App = () => {
+  useEffect(() => {
+    const requestInterceptor = axios.interceptors.request.use((config) => {
+      const token = localStorage.getItem('token');
+      config.headers.Authorization = `Bearer ${token}`;
+
+      return config;
+    });
+    return () => {
+      axios.interceptors.request.eject(requestInterceptor);
+    };
+  }, []);
+
   return (
     <div className="App">
       <UserProvider>
